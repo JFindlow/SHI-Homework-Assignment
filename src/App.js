8f4b85fe77abc451e51be9935130a5e9b6a949be
TheFindog <findlo38@students.rowan.edu>
2022-04-29 13:18:20 -0400

    Changed some variables and polished format

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import brewData from './brewData';
 import './App.css'
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       items: [],
@@ -11,55 +11,54 @@ class App extends Component {
     }
   }
 
-sortBy(sortedBy){
-  let items = this.state.items
-  items.sort(function(a,b){
-    var keyA = a[sortedBy]
-    var keyB = b[sortedBy]
-    if(keyA < keyB) return -1;
-    if(keyA > keyB) return 1;
-    return 0
-  })
-  this.setState({items: items})
-}
-
-render(){
-  if(!this.state.gatheredData){
-    let bro = new brewData().getData();
-    bro.then(json => {
-      this.setState({items: json,
-                      gatheredData: true})
-      
+  sortBy(sortedBy) {
+    let items = this.state.items
+    items.sort(function (a, b) {
+      var keyA = a[sortedBy]
+      var keyB = b[sortedBy]
+      if (keyA < keyB) return -1;
+      if (keyA > keyB) return 1;
+      return 0
     })
+    this.setState({ items: items })
   }
 
-  var items = this.state.items;
-  return (
-    <div className="App">
-      <div className='btn-group'>
-      <button onClick={() => this.sortBy("name")}>Name</button>
-      <button onClick={() => this.sortBy("city")}>City</button>
-      <button onClick={() => this.sortBy("brewery_type")}>Type</button>
+  render() {
+    if (!this.state.gatheredData) {
+      let data = new brewData().getData();
+      data.then(json => {
+        this.setState({
+          items: json,
+          gatheredData: true
+        })
+      })
+    }
+    var items = this.state.items;
+    return (
+      <div className="App">
+        <div className='btn-group'>
+          <button onClick={() => this.sortBy("name")}>Name</button>
+          <button onClick={() => this.sortBy("city")}>City</button>
+          <button onClick={() => this.sortBy("brewery_type")}>Type</button>
+        </div>
+        <div className='data'>
+          <ol>
+            {items.map(item => (
+              <li key={item.id}>
+                <b>Name</b>: {(item.name === null ? 'N/A' : item.name)}<br />
+                <b>Type</b>: {(item.brewery_type === null ? 'N/A' : item.brewery_type)}<br />
+                <b>Address</b>: {(item.street === null ? 'N/A, ' : item.street + ', ')}
+                {(item.city === null ? 'N/A, ' : item.city + ', ')}
+                {item.state}
+                {(item.postal_code === null ? ' N/A' : ' ' + item.postal_code)}<br />
+                <b>Phone</b>: {(item.phone === null ? 'N/A' : item.phone)}<br />
+                <b>Website</b>: {(item.website_url === null ? 'N/A' : item.website_url)}
+              </li>
+            ))}
+          </ol>
+        </div>
       </div>
-      <div className='data'>
-      <ol>
-        {items.map(item => (
-          <li key={item.id}>
-            <b>Name</b>: {(item.name===null ? 'N/A' : item.name)}<br/>
-            <b>Type</b>: {(item.brewery_type===null ? 'N/A' : item.brewery_type)}<br/>
-            <b>Address</b>: {(item.street===null ? 'N/A, ' : item.street + ', ')}
-                            {(item.city===null ? 'N/A, ' : item.city + ', ')}
-                            {item.state} 
-                            {(item.postal_code===null ? ' N/A' : ' ' + item.postal_code)}<br/>
-            <b>Phone</b>: {(item.phone===null ? 'N/A' : item.phone)}<br/>
-            <b>Website</b>: {(item.website_url===null ? 'N/A' : item.website_url)}
-          </li>
-        ))}
-      </ol>
-      </div>
-      
-    </div>
-  );
-}
+    );
+  }
 }
 export default App;
--- src/brewData.js
+++ src/brewData.js
@@ -1,34 +1,36 @@
 import { Component } from "react";
 
-export default class brewData extends Component{
-    constructor(){
+export default class brewData extends Component {
+    constructor() {
         super();
         this.state = JSON.parse(window.localStorage.getItem('state')) || {
-          items: [],
-          timeLoaded: 1,
+            items: [],
+            timeLoaded: 1,
         }
-      }
-    
+    }
+
     setState(state) {
         window.localStorage.setItem('state', JSON.stringify(state))
         super.setState(state)
     }
 
-    async getData(){
+    async getData() {
         var time = new Date().getTime();
-        if((time - this.state.timeLoaded) / (1000 * 60 * 60 * 24) >= 1){
+        if ((time - this.state.timeLoaded) / (1000 * 60 * 60 * 24) >= 1) {
             let data = []
-            for(let i = 1; i < 10; i++){
-                var url = 'https://api.openbrewerydb.org/breweries?by_state=texas&per_page=50&page='+i+'&sort=name:asc'
+            for (let i = 1; i < 10; i++) {
+                var url = 'https://api.openbrewerydb.org/breweries?by_state=texas&per_page=50&page=' + i + '&sort=name:asc'
                 data.push(fetch(url).then(res => res.json()))
             }
-            
-            const merge = [].concat.apply([], await Promise.all(data));
-            this.setState({items: merge,
-                        timeLoaded: time})
+
+            const merge = [].concat.apply([], await Promise.all(data))
+            console.log(merge)
+            this.setState({
+                items: merge,
+                timeLoaded: time
+            })
+
         }
         return this.state.items
-          
     }
-    
 }
